4b9c838dbb7691e19613cbd6f4b2582bc9a74bc7
Douglas Fortunato <douglasfms@gmail.com>
2024-12-26 21:19:30 -0300

    Adicionando um comentário teste

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
-
+//Função para exibir o texto da tela inicial (Comentário teste)
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do número secreto');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
